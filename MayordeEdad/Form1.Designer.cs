f202908ac4eac5387e2b49ee9c3df14496d87902
YeniferAcevedo <yeniferacevedo@hotmail.com>
2020-08-24 21:36:16 -0500

    pprograma que determina si la persona que ingresa la edad es mayor de edad o no

---
+++ MayordeEdad/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MayordeEdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 22F);
+            this.label1.Location = new System.Drawing.Point(106, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Qué edad tienes?";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtEdad.Font = new System.Drawing.Font("Arial", 16F);
+            this.txtEdad.Location = new System.Drawing.Point(89, 139);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 32);
+            this.txtEdad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.Location = new System.Drawing.Point(85, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Escriba la edad y presione consultar";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold);
+            this.btnConsultar.Location = new System.Drawing.Point(309, 135);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(91, 36);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Arial", 12F);
+            this.lblRespuesta.Location = new System.Drawing.Point(174, 201);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(83, 18);
+            this.lblRespuesta.TabIndex = 5;
+            this.lblRespuesta.Text = "Respuesta";
+            this.lblRespuesta.Visible = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold);
+            this.btnLimpiar.Location = new System.Drawing.Point(190, 253);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(91, 36);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(513, 325);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mayor de Edad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ MayordeEdad/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayordeEdad
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            double edad;
+
+            edad = double.Parse(txtEdad.Text);
+
+            if (edad > 18) {
+                lblRespuesta.Text = ("Eres mayor de edad");
+                
+            }
+            else if (edad > 1 && edad <= 18)  {
+                lblRespuesta.Text = ("Eres menor de edad");
+            }
+            else if (edad <= 0) {
+                lblRespuesta.Text= ("La edad ingresada no es permitida");
+             }
+            lblRespuesta.Visible = true;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtEdad.Text = string.Empty;
+            lblRespuesta.Text = " ";
+            txtEdad.Focus();
+        }
+    }
+}
